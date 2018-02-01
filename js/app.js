ff6020e035ff15821e8d71e13a1a78bfa539f15e
MoJoe <joeypclausen@gmail.com>
2018-02-01 14:08:28 -0600

    stubbed in routes, and directories for views, controllers, and images.

--- js/app.js
+++ js/app.js
@@ -7,6 +7,14 @@
             controller : 'MainCtrl',
             templateUrl : 'views/home.html'
         })
+        .when('/weather',{
+            controller : 'WeatherCtrl',
+            templateUrl : 'views/weather.html'
+        })
+        .when('/menu',{
+            controller : 'MenuCtrl',
+            templateUrl : 'views/menu.html'
+        })
         .otherwise({ redirectTo : '/' });
     });
     
